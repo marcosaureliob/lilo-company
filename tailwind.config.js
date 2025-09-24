82e0ff6f59f441045cd9dfff735790f795e3399c
Marcos <marcos.braz@grt8.com.br>
2025-09-23 21:37:45 -0300

    feat: enhance login page layout and add form fields for username and password

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        'xxl': '1400px',
+        '2xl': '1536px',
+      },
     },
   },
 };
